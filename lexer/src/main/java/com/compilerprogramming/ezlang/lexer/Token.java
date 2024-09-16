a13b9551621de7abb4a8769a324696240708346c
Dibyendu Majumdar <dibyendumajumdar@users.noreply.github.com>
2024-09-16 09:29:35 +0100

    Update Token.java

--- lexer/src/main/java/com/compilerprogramming/ezlang/lexer/Token.java
+++ lexer/src/main/java/com/compilerprogramming/ezlang/lexer/Token.java
@@ -1,19 +1,3 @@
-/*
- * DO NOT REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- * <p>
- * Contributor(s):
- * <p>
- * The Initial Developer of the Original Software is Dibyendu Majumdar.
- * <p>
- * This file is part of the Compiler Craft project.
- * See https://github.com/dibyendumajumdar/CompilerCraft
- * <p>
- * The contents of this file are subject to the terms of the
- * Apache License Version 2 (the "APL"). You may not use this
- * file except in compliance with the License. A copy of the
- * APL may be obtained from:
- * http://www.apache.org/licenses/LICENSE-2.0
- */
 package com.compilerprogramming.ezlang.lexer;
 
 public class Token {
